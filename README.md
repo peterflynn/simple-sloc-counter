47cd11f10e986d2492331c6600a5f7a6cd6b1efa
Peter Flynn <pflynn@adobe.com>
2013-05-09 11:08:56 -0700

    Initial checkin

---
+++ README.md
@@ -0,0 +1,22 @@
+Source Lines of Code (SLOC) Counter for Brackets
+================================================
+Counts the number of files, lines, and lines of code (excluding whitespace/comments) in your project. You can exclude third-party code such as jQuery by specifying file / folder names.
+
+To get started, just choose _View > Lines of Code Count_
+
+How to Install
+==============
+SLOC Counter is an extension for [Brackets](https://github.com/adobe/brackets/), a new open-source code editor for the web.
+
+To use SLOC Counter:
+
+1. Choose _File > Install Extension_
+2. Enter this URL: _https://github.com/peterflynn/sloc-brackets_
+3. Click _Install_!
+
+
+### License
+MIT-licensed -- see `main.js` for details.
+
+### Compatibility
+Brackets Sprint 15 or newer (or Adobe Edge Code Preview 2 or newer).
---
+++ main.js
@@ -0,0 +1,233 @@
+/*
+ * Copyright (c) 2013 Peter Flynn, Adobe Systems Incorporated, and other contributors.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50, regexp: true */
+/*global define, brackets, $ */
+
+define(function (require, exports, module) {
+    "use strict";
+    
+    // Brackets modules
+    var DocumentManager         = brackets.getModule("document/DocumentManager"),
+        FileIndexManager        = brackets.getModule("project/FileIndexManager"),
+        StatusBar               = brackets.getModule("widgets/StatusBar"),
+        Async                   = brackets.getModule("utils/Async"),
+        Dialogs                 = brackets.getModule("widgets/Dialogs"),
+        Menus                   = brackets.getModule("command/Menus"),
+        CommandManager          = brackets.getModule("command/CommandManager");
+    
+    
+    function filter(fileInfo) {
+        var path = fileInfo.fullPath;
+        if (path.indexOf("/extensions/dev/") !== -1) {  // in THIS case, want to exclude any personal code...
+            return false;
+        }
+        if (path.indexOf("/thirdparty/") !== -1) {
+            return false;
+        }
+        if (path.indexOf("/3rdparty/") !== -1) {
+            return false;
+        }
+        if (path.indexOf("/node_modules/") !== -1) {
+            return false;
+        }
+        if (path.indexOf("/widgets/bootstrap-") !== -1) {
+            return false;
+        }
+        if (path.indexOf("/unittest-files/") !== -1) {
+            return false;
+        }
+        if (path.indexOf("/spec/JSUtils-test-files/") !== -1) {
+            return false;
+        }
+        if (path.indexOf("/perf/OpenFile-perf-files/") !== -1) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    /** Quick way to bail early from _countSloc() */
+    function Unsupported(msg, lineNum) { this.message = msg; this.lineNum = lineNum; }
+    Unsupported.prototype = new Error();
+    
+    
+    /**
+     * Counts lines of code in given text.
+     * @return {{total: number, sloc: number}}
+     */
+    function _countSloc(text) {
+        // TODO: split on /\r\n/g so we can call getText(true) for speed?
+        var lines = text.split("\n");
+        
+        var codeLines = 0;
+        var inBlockComment = false;
+        lines.forEach(function (line, lineNum) {
+            function unsupported(msg) {
+                throw new Unsupported(msg, lineNum);
+            }
+            
+            if (inBlockComment) {
+                var commentEnd = line.indexOf("*/");
+                if (commentEnd !== -1) {
+                    inBlockComment = false;
+                    if (line.substr(commentEnd + 2).trim() !== "") {
+                        unsupported("Code on same line as end of multi-line block comment");
+                    }
+                }
+            } else {
+                if (line.match(/^\s*$/)) {
+                    // whitespace
+                } else if (line.match(/^\s*\/\//)) {
+                    // line comment
+                } else if (line.indexOf("/*") !== -1) {
+                    var commentStart = line.indexOf("/*");
+                    var beforeCommentStart = line.substring(0, commentStart);
+                    if (beforeCommentStart.trim() !== "") {
+                        codeLines++;  // block comment after some code still counts as a line of code: e.g. 'foo(); /* call foo */'
+                        if (beforeCommentStart.indexOf("\"") !== -1 || beforeCommentStart.indexOf("'") !== -1) {
+                            unsupported("Block comment token that may be inside a string literal");
+                        }
+                        if (line.indexOf("*/") === -1) {
+                            unsupported("Code on same line as start of multi-line block comment");
+                        }
+                        
+                    }
+                    if (line.indexOf("*/") === -1) {
+                        inBlockComment = true;
+                    }
+                    if (line.indexOf("//") !== -1) {
+                        unsupported("Mixing block and line comments on one line");
+                    }
+                    if (line.indexOf("/*", commentStart + 2) !== -1) {
+                        unsupported("Multiple block comments per line");
+                    }
+                } else {
+                    codeLines++;
+                }
+            }
+        });
+        
+            
+        return { total: lines.length, sloc: codeLines };
+    }
+    
+    
+    /**
+     * Finds all JS files, loads all those that pass the filter(), counts lines, and shows final
+     * result in a dialog.
+     */
+    function countAllFiles() {
+        var totalLines = 0,
+            totalSloc = 0,
+            totalBytes = 0,
+            totalFiles = 0;
+        
+        var warnings = [];
+        
+        // Code based on FileIndexManager & a bit of JSUtils
+        
+        StatusBar.showBusyIndicator(true);
+        FileIndexManager.getFileInfoList("all")
+            .done(function (fileListResult) {
+                
+                var jsFiles = fileListResult.filter(function (fileInfo) {
+                    return (/\.js$/i).test(fileInfo.fullPath);
+                });
+                
+                Async.doInParallel(jsFiles, function (fileInfo) {
+                    var result = new $.Deferred();
+                    
+                    if (!filter(fileInfo)) {
+                        result.resolve();
+                    } else {
+                        // Search one file
+                        DocumentManager.getDocumentForPath(fileInfo.fullPath)
+                            .done(function (doc) {
+                                var text = doc.getText();
+                                
+                                try {
+                                    var lineCounts = _countSloc(text);
+                                    totalLines += lineCounts.total;
+                                    totalSloc += lineCounts.sloc;
+                                    totalBytes += text.length;
+                                    totalFiles++;
+                                        
+                                } catch (err) {
+                                    if (err instanceof Unsupported) {
+                                        warnings.push({ reason: err.message, fullPath: fileInfo.fullPath, lineNum: err.lineNum });
+                                    } else {
+                                        var wrap = new Error("Rethrowing: " + err.message);
+                                        wrap.innerException = err;
+                                        throw wrap;
+                                    }
+                                }
+                                
+                                result.resolve();
+                            })
+                            .fail(function (error) {
+                                // Error reading this file
+                                // Resolve anyway so we can still do a partial count
+                                warnings.push({ reason: "Unable to read file", fullPath: fileInfo.fullPath });
+                                result.resolve();
+                            });
+                    }
+                    return result.promise();
+                })
+                    .always(function () {
+                        StatusBar.hideBusyIndicator();
+                    })
+                    .done(function () {
+                        // Done processing all files: show results
+                        var totalKb = Math.round(totalBytes / 1024);
+                        var message = "<div style='-webkit-user-select:text; cursor: auto'>";
+                        
+                        message +=
+                            "Scanned " + totalFiles + " files (" + totalKb + " KB).<br>" +
+                            "Raw total lines: " + totalLines + "<br>" +
+                            "<b>Lines of code: " + totalSloc + "</b>&nbsp; (excluding whitespace & comments)";
+                        
+                        if (warnings.length) {
+                            message += "<div style='border:1px solid #dfb200; background-color: #fffad8; margin-top:20px; padding:10px; max-height:250px; overflow:auto'>";
+                            warnings.forEach(function (warning) {
+                                message += "Ignored '" + warning.fullPath + "': " + warning.reason + " at line " + (warning.lineNum + 1) + "<br>";
+                            });
+                            message += "</div>";
+                        }
+                        
+                        message += "</div>";
+                        
+                        Dialogs.showModalDialog(Dialogs.DIALOG_ID_ERROR, "Lines of Code", message);
+                    });
+            });
+    }
+    
+    
+    
+    // Register command
+    var COMMAND_ID = "pflynn.count_sloc";
+    CommandManager.register("Lines of Code Count", COMMAND_ID, countAllFiles);
+    
+    var menu = Menus.getMenu(Menus.AppMenuBar.VIEW_MENU);
+    menu.addMenuItem(COMMAND_ID, null, Menus.LAST);
+});
